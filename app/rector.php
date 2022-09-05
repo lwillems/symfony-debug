9c080b164fb643a70b6d89c4f952a24fe62acbe9
Laurent WILLEMS <willems.laurent@gmail.com>
2022-09-05 11:09:06 +0200

    rector sf6

---
+++ app/rector.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->symfonyContainerXml(        __DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
+    // endregion
+    $rectorConfig->import(SymfonySetList::SYMFONY_60);
+    $rectorConfig->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+    $rectorConfig->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
+};
